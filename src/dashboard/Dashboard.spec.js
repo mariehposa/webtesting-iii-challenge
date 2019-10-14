4d244df6197a84c15f563e2d635ec0e46d24946f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-14 14:12:38 +0100

    add test for dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -12,3 +12,26 @@ beforeEach(() => {
     wrapper = rtl.render(<Dashboard />)
 })
 
+describe('dashboard component', () => {
+
+    it('defaults to unlocked', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument()
+    })
+
+    it('defaults to open', () => {
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument()
+    })
+
+    it('cannot be closed or opened when locked', () => {
+        let gateClose = wrapper.queryByText(/close gate/i)
+        let gateLock = wrapper.queryByText(/lock gate/i)
+
+        expect(wrapper.queryByText(/closed/i)).not.toBeInTheDocument()
+        rtl.fireEvent.click(gateClose)
+        expect(wrapper.queryByText(/closed/i)).toBeInTheDocument()
+
+        rtl.fireEvent.click(gateLock)
+        expect(wrapper.queryByText('Open')).not.toBeInTheDocument()
+        expect(wrapper.queryByText(/unlock Gate/i)).toBeInTheDocument()
+    })
+})
