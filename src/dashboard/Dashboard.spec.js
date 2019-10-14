60cef2c416e5d5c3f2eab6d5d1960959a5e6b892
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-14 13:35:50 +0100

    set up test for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+let wrapper, incSyncButton;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Dashboard />)
+})
+
