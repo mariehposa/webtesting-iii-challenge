c912d7f90e363fc8e43b3a33050671b719fb2b50
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-14 14:20:22 +0100

    set up test for display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+
+import React from React;
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper
+
+beforeEach(() => {
+    wrapper = rtl.render(<Display />)
+})
