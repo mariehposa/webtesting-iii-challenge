18762cab41b6c084d7b5f69d56fa889a3956cb18
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-14 19:58:25 +0100

    test to displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,17 +10,26 @@ afterEach(rtl.cleanup);
 let wrapper
 
 beforeEach(() => {
-    wrapper = rtl.render(<Display />)
+    // wrapper = rtl.render(<Display />)
 })
 
 describe('Display component', () => {
 
     it('gate is opened', () => {
+        wrapper = rtl.render(<Display />)
         expect(wrapper.queryByText(/open/i)).toBeInTheDocument()
     })
     it('gate is unlocked', () => {
+        wrapper = rtl.render(<Display />)
         expect(wrapper.queryByText(/unlock/i)).toBeInTheDocument()
     })
     
-    
+    test('display closed if closed prop is true', () => {
+        wrapper = rtl.render(<Display closed = {true} />)
+        expect(wrapper.queryByText(/closed/i)).toBeInTheDocument()
+    })
+    test('display open if closed prop is false', () => {
+        wrapper = rtl.render(<Display closed = {false}/>)
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument()
+    })
 })
