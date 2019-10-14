8efd23b585c76fe1f4b2ab404f2139f14fea515d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-14 21:08:41 +0100

    test to provide buttons to toggle the `closed` and `locked` states.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Control from './Controls';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Control />)
+})
+
+describe('dashboard component', () => {
+    test('provide button to toggle to closed and locked states', () => {
+        expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument()
+        expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument()
+    })
+})
