6635d3d21d826a74ff4ff6ccba294585306b73c1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-15 08:59:42 +0100

    testing buttons

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,6 +4,7 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Control from './Controls';
+import { fireEvent } from '@testing-library/react';
 
 afterEach(rtl.cleanup);
 
@@ -17,5 +18,14 @@ describe('dashboard component', () => {
     test('provide button to toggle to closed and locked states', () => {
         expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument()
         expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument()
+    })
+
+    test('button text changes when clicked', () => {
+        let closeGate = wrapper.queryByText(/close gate/i)
+        let lockGate = wrapper.queryByText(/lock gate/i)
+
+        rtl.fireEvent.click(closeGate)
+        wrapper.debug()
+        expect(wrapper.queryByText(/Open Gate/i)).toBeInTheDocument()
     })
 })
